3f16ef1a0c8ec70c5c8eba296bc806334a47ba3e
unknown <orblanc@cotecolisson.com>
2015-01-31 12:49:10 +0100

    Premier commit -addition

---
+++ FloKWS/IService1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FloKWS
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        List<Business> GetBusinesses();
+
+        [OperationContract]
+        int ReturnResultAddition(int first, int second);
+    }
+
+
+    [DataContract]
+    public class Business
+    {
+        //You can add any fields you would like to include
+        bool isOpen = true;
+        string name;
+        string logoUri;
+
+        [DataMember]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [DataMember] 
+        public string LogoUri
+        {
+            get { return logoUri; }
+            set { logoUri = value; }
+        }
+
+        [DataMember]
+        public bool IsOpen
+        {
+            get { return isOpen; }
+            set { isOpen = value; }
+        }
+    }
+}
---
+++ FloKWS/Service1.svc.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace FloKWS
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez Service1.svc ou Service1.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class Service1 : IService1
+    {
+
+
+
+
+        //public string GetData(int value)
+        //{
+        //    return string.Format("You entered: {0}", value);
+        //}
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+        public List<Business> GetBusinesses()
+        {
+            var Businesses = new List<Business>();
+            Businesses.Add(new Business() { IsOpen = true, LogoUri = "http://www.christianjvella.com/logosample1.png", Name = "Wine & Dine" });
+            Businesses.Add(new Business() { IsOpen = true, LogoUri = "http://www.christianjvella.com/logosample2.png", Name = "Wine Bar" });
+            Businesses.Add(new Business() { IsOpen = false, LogoUri = "http://www.christianjvella.com/logosample3.png", Name = "Restaurant" });
+            Businesses.Add(new Business() { IsOpen = true, LogoUri = "http://www.christianjvella.com/logosample4.png", Name = "Sports Bar" });
+            Businesses.Add(new Business() { IsOpen = false, LogoUri = "http://www.christianjvella.com/logosample5.png", Name = "Cinema" });
+            return Businesses;
+        }
+
+
+        public int ReturnResultAddition(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
